0fe59d447300d9890b88eafb28660b3b09f915b1
Rishabh Chaurasia <chaurasia.rishabh7@gmail.com>
2021-02-07 23:17:55 +0300

    Adding OCR feature

---
+++ ocr.py
@@ -0,0 +1,10 @@
+import cv2
+import pytesseract
+import numpy as np
+from PIL import ImageGrab
+import time
+
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+img = cv2.imread('1.png')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
