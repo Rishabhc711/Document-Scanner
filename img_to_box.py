2653b70e3e4eef5bff483c545307ccf7cde127e8
Rishabh Chaurasia <chaurasia.rishabh7@gmail.com>
2021-03-10 00:01:41 +0300

    Image to box"

--- img_to_box.py
+++ img_to_box.py
@@ -1,3 +1,6 @@
+import numpy as np
+from PIL import ImageGrab
+import time
 import cv2
 import pytesseract
 
@@ -13,5 +16,16 @@ cv2.waitKey(0)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 cv2.imshow("after colour conversion", img)
 print(pytesseract.image_to_string(img))
+
+#print(pytesseract.image_to_boxes(img))
+boxes = pytesseract.image_to_boxes(img)
+for b in boxes.splitlines():
+    b=b.split(' ')
+    print(b)
+    x,y,w,h=int(b[1]),int(b[2]),int(b[3]),int(b[4])
+    cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 2)
+
+
+cv2.imshow("input",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
