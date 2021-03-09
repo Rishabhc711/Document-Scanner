a69fc7fa7ea656b73a19e0ce15704434ea7a7722
Rishabh Chaurasia <chaurasia.rishabh7@gmail.com>
2021-03-09 23:20:44 +0300

    updating OCR

--- ocr.py
+++ ocr.py
@@ -4,7 +4,15 @@ import numpy as np
 from PIL import ImageGrab
 import time
 
-
+heightImg = 640
+widthImg  = 480
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-img = cv2.imread('1.png')
+img = cv2.imread('Images/20210114_230948.jpg')
+img = cv2.resize(img, (widthImg, heightImg))
+print(img)
+cv2.imshow("input",img)
+cv2.waitKey(0)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+cv2.imshow("after colour conversion", img)
+cv2.waitKey(0)
+print('hello')
